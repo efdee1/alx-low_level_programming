548ebcb1a45b06bb50a3597dc6f0d9dc63b5d91c
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-10 07:47:41 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  *
  * main - Entry point
@@ -10,6 +12,6 @@
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
