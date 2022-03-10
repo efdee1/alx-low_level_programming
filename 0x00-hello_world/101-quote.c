c1e9ab002964647d023d57b743a032b59ed03171
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-10 13:34:05 -0800

    alx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
+ *
  * main - Entry point
- * Return:1
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Return: 1
+ *
  */
+
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
