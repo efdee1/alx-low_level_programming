c6d52ba3695ce3606dba53d55d0d3e128dc37ba5
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-11 15:00:01 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
