891ba85ca2b589edd4f7434a7e11b60554eecb4d
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-12 03:50:08 -0800

    20mph

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,8 +20,10 @@ int main(void)
 	for (i = 0; i < 1000; i++)
 	{
 		j = i / 100; /* hundreds */
-		k = (i / 10) % 10; /*tens */
-		l = i % 100; /* singles */
+
+		k = (i / 10) % 10; /* tens */
+
+		l = i % 10; /* singles */
 
 		if (j < k && k < l)
 		{
