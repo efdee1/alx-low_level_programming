9018c778bc18d6f2d15f18f1d8497c0344b96d60
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-11 23:03:46 -0800

    alplABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main: main block
+ * main - main block
  *
- * Description: putchar
+ * Description: Use 'putchar' to print all letters but the letter 'q' and 'e'
  *
  * Return: 0
  */
