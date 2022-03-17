27e8adca879d5470f6a8996ce8a43acdf3b02875
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-17 15:09:54 -0700

    101print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,30 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
 	if (n < 0)
 	{
+		n = n * -1;
 		putchar('-');
-		num = -num;
 	}
+	num = n;
+	num2 = num;
+	if (num > 9)
+	{
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		putchar((num2 / aux) + '0');
+		aux = aux / 10;
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	putchar((num % 10) + '0');
+		for (i = aux; i >= 1; i = i / 10)
+			putchar((num2 / i) % 10 + '0');
+	}
+	else
+		putchar(num + '0');
 }
