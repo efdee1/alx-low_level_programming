3bf89c1e73fa262eecfac52a2133fa919882a37f
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-17 10:30:43 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * print_numbers - Prints the numbers from 0-9.
+ * @void: no return
  */
 void print_numbers(void)
 {
-	int num;
+int num;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
 
-	_putchar('\n');
+_putchar('\n');
 }
