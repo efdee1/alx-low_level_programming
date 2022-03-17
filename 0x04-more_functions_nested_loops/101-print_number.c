7a129c7624b349a532b91bad11172736d08a2a5b
Efdee1 <feranmi.adejinmi@gmail.com>
2022-03-17 15:55:13 -0700

    12th

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -25,12 +25,12 @@ void print_number(int n)
 			aux = aux * 10;
 			num = num / 10;
 		}
-		_putchar((num2 / aux) + '0');
+		putchar((num2 / aux) + '0');
 		aux = aux / 10;
 
 		for (i = aux; i >= 1; i = i / 10)
-			_putchar((num2 / i) % 10 + '0');
+			putchar((num2 / i) % 10 + '0');
 	}
 	else
-		_putchar(num + '0');
+		putchar(num + '0');
 }
