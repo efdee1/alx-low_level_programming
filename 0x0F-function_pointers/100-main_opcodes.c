7ce3f1d1dfc64299f51984be20b2cfb63984b8c4
efdee1 <63516254+efdee1@users.noreply.github.com>
2022-04-13 21:26:00 +0100

    opdecs

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,29 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_sub -
+ *main - a
+ *@argc: a
+ *@argv: a
  *
- * Return: 
+ *Return: a
  */
-int op_sub(int a, int b)
+int main(int argc, char *argv[])
 {
+	int num_of_bytes;
 
+	if (argc != 2)
+	{
+		printf("Error");
+		exit(1);
+	}
+	num_of_bytes = atoi(argv[1]);
+	if (num_of_bytes < 0)
+	{
+		printf("Error");
+		exit(2);
+	}
+        system("gcc 100-main_opcodes.c -c");
+	system("objdump -D 100-main_opcodes.o");
+	return (0);
 }
